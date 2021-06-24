6267668715fdc7bad7ffd40471ce14f23ce7581c
tamil01 <tamil@toverto.com>
2021-06-24 16:42:37 +0530

    Add files via upload

--- winextension.ps1
+++ winextension.ps1
@@ -145,15 +145,9 @@ Install-Binary -Url $InstallerUrl -Name $InstallerName
 ################################################################################
 ##  Desc:  Install SQL PowerShell tool
 ################################################################################
-# $BaseUrl = "https://download.microsoft.com/download/B/1/7/B1783FE9-717B-4F78-A39A-A2E27E3D679D/ENU/x64"
-# # install required MSIs
-# $SQLSysClrTypesName = "SQLSysClrTypes.msi"
-# $SQLSysClrTypesUrl = "${BaseUrl}/${SQLSysClrTypesName}"
-# Install-Binary -Url $SQLSysClrTypesUrl -Name $SQLSysClrTypesName
-# $SharedManagementObjectsName = "SharedManagementObjects.msi"
-# $SharedManagementObjectsUrl = "${BaseUrl}/${SharedManagementObjectsName}"
-# Install-Binary -Url $SharedManagementObjectsUrl -Name $SharedManagementObjectsName
-# $PowerShellToolsName = "PowerShellTools.msi"
-# $PowerShellToolsUrl = "${BaseUrl}/${PowerShellToolsName}"
-# Install-Binary -Url $PowerShellToolsUrl -Name $PowerShellToolsName
+$AdalsqlBaseUrl = "https://download.microsoft.com/download/6/4/6/64677D6E-06EA-4DBB-AF05-B92403BB6CB9/ENU/x64"
+$AdalsqlName = "adalsql.msi"
+$AdalsqlUrl = "${AdalsqlBaseUrl}/${AdalsqlName}"
+Install-Binary -Url $AdalsqlUrl -Name $AdalsqlName
+
 Install-Module -Name SqlServer -RequiredVersion 21.1.18245
