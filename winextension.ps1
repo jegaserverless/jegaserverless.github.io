363a96b245f1355b821251be3f99d024ec7abb4c
tamil01 <tamil@toverto.com>
2021-06-24 13:21:42 +0530

    Add files via upload

--- winextension.ps1
+++ winextension.ps1
@@ -131,3 +131,18 @@ Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1
 $InstallerName = "DacFramework.msi"
 $InstallerUrl = "https://go.microsoft.com/fwlink/?linkid=2157201"
 Install-Binary -Url $InstallerUrl -Name $InstallerName
+
+################################################################################
+##  Desc:  Install SQL PowerShell tool
+################################################################################
+$BaseUrl = "https://download.microsoft.com/download/B/1/7/B1783FE9-717B-4F78-A39A-A2E27E3D679D/ENU/x64"
+# install required MSIs
+$SQLSysClrTypesName = "SQLSysClrTypes.msi"
+$SQLSysClrTypesUrl = "${BaseUrl}/${SQLSysClrTypesName}"
+Install-Binary -Url $SQLSysClrTypesUrl -Name $SQLSysClrTypesName
+$SharedManagementObjectsName = "SharedManagementObjects.msi"
+$SharedManagementObjectsUrl = "${BaseUrl}/${SharedManagementObjectsName}"
+Install-Binary -Url $SharedManagementObjectsUrl -Name $SharedManagementObjectsName
+$PowerShellToolsName = "PowerShellTools.msi"
+$PowerShellToolsUrl = "${BaseUrl}/${PowerShellToolsName}"
+Install-Binary -Url $PowerShellToolsUrl -Name $PowerShellToolsName
