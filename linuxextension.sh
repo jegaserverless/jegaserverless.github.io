15c368a9921623ba26d91aba61d40745a500847a
tamil01 <tamil@toverto.com>
2021-06-26 18:02:23 +0530

    Add files via upload

--- linuxextension.sh
+++ linuxextension.sh
@@ -171,6 +171,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update&&sudo add-apt-repository universe&&sudo apt-get install -y powershell
+sudo apt-get install -y parallel
 
 # Ubuntu 20 doesn't support EOL versions
 LATEST_DOTNET_PACKAGES=$(get_toolset_value '.dotnet.aptPackages[]')
