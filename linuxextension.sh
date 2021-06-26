ccc36d6b7f6c5f3cb0b3a05cd44af235b7f7c359
tamil01 <tamil@toverto.com>
2021-06-26 17:44:49 +0530

    Add files via upload

--- linuxextension.sh
+++ linuxextension.sh
@@ -166,7 +166,7 @@ getOSVersionLabel()
 }
 
 
-sudo apt-get update && sudo apt-get install jq
+sudo apt-get update && sudo apt-get install -y jq
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
