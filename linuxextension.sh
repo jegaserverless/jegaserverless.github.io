11755a63b82e37a153089883ee204fb761ba7373
tamil01 <tamil@toverto.com>
2021-06-26 15:39:58 +0530

    Add files via upload

--- linuxextension.sh
+++ linuxextension.sh
@@ -2,6 +2,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update&&sudo add-apt-repository universe&&sudo apt-get install -y powershell
-curl -sSL https://dot.net/v1/dotnet-install.sh | sudo bash /dev/stdin -c Current
-curl -sSL https://dot.net/v1/dotnet-install.sh | sudo bash /dev/stdin -c LTS
-export PATH="$PATH:$HOME/.dotnet" | sudo bash
+sudo snap install dotnet-sdk --classic --channel=5.0
+sudo snap install dotnet-sdk --classic --channel=3.1
+sudo snap install dotnet-sdk --classic --channel=2.1
+sudo snap alias dotnet-sdk.dotnet dotnet
