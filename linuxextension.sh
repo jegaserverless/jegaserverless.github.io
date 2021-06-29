f0fb568e7683721a9b2abbbe2b9e053ec921ff14
tamil01 <tamil@toverto.com>
2021-06-29 08:00:21 +0530

    Add files via upload

--- linuxextension.sh
+++ linuxextension.sh
@@ -185,8 +185,6 @@ sudo apt-get update
 sudo add-apt-repository universe
 # Install PowerShell
 sudo apt-get install -y powershell
-sudo apt-get update
-sudo pwsh -c "Install-Module -Name Az -Force"
 # Install jq for JSON parsing in functions
 sudo apt-get install -y jq
 # Install parallel for parallel installation
@@ -260,3 +258,7 @@ sh get-docker.sh
 URL=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[].browser_download_url | select(endswith("docker-compose-Linux-x86_64"))')
 curl -L $URL -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Installing Az module to powershell
+sudo apt-get update
+sudo pwsh -c "Install-Module -Name Az -Force -AllowClobber"
